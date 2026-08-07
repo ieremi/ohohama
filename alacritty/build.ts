a369243c7015078c61b515e9cdd9a82271292746
ieremius <hibariyashiki@gmail.com>
2026-08-07 12:31:26 +0900

    build

--- alacritty/build.ts
+++ alacritty/build.ts
@@ -1,9 +1,18 @@
 import { stringify } from "smol-toml";
-import { writeFile } from "node:fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
+import { dirname } from "node:path";
 import { config } from "./config.js";
 
+const OUTPUT_PATH =
+    "./alacritty/themes/ohohama.toml";
+
+await mkdir(
+    dirname(OUTPUT_PATH),
+    { recursive: true },
+);
+
 await writeFile(
-    "./alacritty/themes/ohohama.toml",
+    OUTPUT_PATH,
     stringify(config),
     "utf8",
 );
